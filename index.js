fec4d1c36e86b87f24c5c31d3283e98398f16bd3
klpabes <121295353+klpabes@users.noreply.github.com>
2025-04-09 13:12:03 +0800

    Add card functionality

--- index.js
+++ index.js
@@ -2,28 +2,94 @@
 const myLibrary = [];
 
 const contentSelector = document.querySelector(".content");
+const addNewBookBtn = document.querySelector("#newbook");
 
-function Book(title, author, pages, desc) {
+const dialog = document.querySelector("#favDialog");
+const dialogForm = document.querySelector(".bookForm");
+
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const inputPages = document.querySelector("#pages");
+const inputDesc = document.querySelector("#desc");
+const inputRead = document.querySelector("#read");
+
+const closeBtn = document.querySelector("#closeBtn");
+const confirmBtn = document.querySelector("#confirmBtn");
+
+function Book(title, author, pages, desc, read) {
   // the constructor...
   this.title = title;
-  this.author = this.author;
-  this.pages = this.pages;
+  this.author = author;
+  this.pages = pages;
   this.desc = desc ? desc : "No description";
   this.id = crypto.randomUUID();
+  this.read = read;
+
+  this.changeRead = function () {
+    this.read = !this.read;
+  };
 }
 
-function addBookToLibrary(name) {
-  // take params, create a book then store it in the array
+function addBookToLibrary(title, author, pages, desc, read) {
+  const book = new Book(title, author, pages, desc, read);
 
-  const book = new Book(name);
   myLibrary.push(book);
 }
 
 function displayBook() {
-  for (const book of myLibrary) {
+  contentSelector.innerHTML = "";
+  myLibrary.forEach(function (book, index, arr) {
     contentSelector.insertAdjacentHTML(
       "beforeend",
-      `<article>${book.name}</article>`
+      ` <article data-index=${index}>
+            <h3 id="title">Title: ${book.title}</h3>
+            <h3 id="author">Author: ${book.author}</h4>
+            <h3 id="pages">Pages: ${book.pages}</h3>
+            <h3>Description</h3>
+            <p>${book.desc}</p>
+            <div>
+              <button onclick="readBook(${index})" class="btn-read ${
+        book.read ? "read" : ""
+      }">Read</button>
+              <button onclick="removeBook(${index})" class="btn-remove">Remove</button>     
+            </div>
+        </article>
+      `
     );
-  }
+  });
+}
+
+closeBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  dialog.close();
+});
+
+confirmBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  addBookToLibrary(
+    inputTitle.value,
+    inputAuthor.value,
+    inputPages.value,
+    inputDesc.value,
+    inputRead.checked
+  );
+
+  dialogForm.reset();
+  dialog.close();
+  displayBook();
+});
+
+addNewBookBtn.addEventListener("click", function (e) {
+  dialog.showModal();
+});
+
+function removeBook(book) {
+  myLibrary.splice(book, 1);
+  displayBook();
+}
+
+function readBook(book) {
+  myLibrary[book].changeRead();
+  displayBook();
 }
